a08a3809017d783a58903ee8b0f5b8b2c72bdeb8
wonder woman <rdelquiam@gmail.com>
2019-01-17 12:49:16 -0800

    working app

--- app/model/friends.js
+++ app/model/friends.js
@@ -1,23 +1,8 @@
 module.exports =[
-    {
-        "name": "Rhea",
-        "photo": "https://www.deviantart.com/loish/art/Wonder-Woman-649767879",
-        "scores": [
-            "4",
-            "4",
-            "3",
-            "3",
-            "1",
-            "2",
-            "2",
-            "5",
-            "2",
-            "4"
-        ]
-    },
+    
     {
         "name": "Carrie",
-        "photo": "",
+        "photo": "https://cdn.vox-cdn.com/thumbor/s4QSszQzFYQCLV5pFjVbEkfmA4w=/42x0:691x433/1220x813/filters:focal(42x0:691x433):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/51254459/Screen_Shot_2016-10-07_at_5.21.15_PM.0.0.png",
         "scores": [
             "5",
             "4",
@@ -33,7 +18,7 @@ module.exports =[
     },
     {
         "name": "Samantha",
-        "photo": "",
+        "photo": "https://media.self.com/photos/57d89db350778cef321a4d18/4:3/w_752,c_limit/samantha-jones.png",
         "scores": [
             "4",
             "5",
@@ -49,7 +34,7 @@ module.exports =[
     },
     {
         "name": "Miranda",
-        "photo": "",
+        "photo": "https://imgix.bustle.com/rehost/2016/9/13/d90659a6-6628-4d70-8ae0-1aecb8f5a11f.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
         "scores": [
             "1",
             "2",
@@ -65,7 +50,7 @@ module.exports =[
     },
     {
         "name": "Charlotte",
-        "photo": "",
+        "photo": "http://4.bp.blogspot.com/-ix4V4X8ksGo/TjZVG0sQ-2I/AAAAAAAAJvk/NNyYxj3K5gs/s400/charlotte-york.jpg",
         "scores": [
             "1",
             "3",
@@ -78,53 +63,6 @@ module.exports =[
             "3",
             "1"
         ]
-    },
-    {
-        "name": "Madison",
-        "photo": "https://www.deviantart.com/loish/art/Wonder-Woman-649767879",
-        "scores": [
-            "1",
-            "3",
-            "5",
-            "2",
-            "4",
-            "4",
-            "2",
-            "5",
-            "3",
-            "1"
-        ]
-    },
-    {
-        "name": "Madison",
-        "photo": "https://www.deviantart.com/loish/art/Wonder-Woman-649767879",
-        "scores": [
-            "1",
-            "3",
-            "5",
-            "2",
-            "4",
-            "4",
-            "2",
-            "5",
-            "3",
-            "1"
-        ]
-    },
-    {
-        "name": "Madison",
-        "photo": "https://www.deviantart.com/loish/art/Wonder-Woman-649767879",
-        "scores": [
-            "1",
-            "3",
-            "5",
-            "2",
-            "4",
-            "4",
-            "2",
-            "5",
-            "3",
-            "1"
-        ]
+    
     }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -30,37 +30,51 @@ module.exports = function (app) {
     // ...the JSON is pushed to the appropriate JavaScript array
 
     app.post("/api/friends",  (req, res) => {
-        console.log(req.body);
+        // console.log(req.body);
         // steps needed to achieve finding a friend
         // compare users answers with friends array
         // find the most compatible friend
         // res.json compatible friend
-        var newShopper = req.body;
-        var newFriend = {};
+        // var newShopper = req.body;
+        // var newFriend = {};
+        var newFriend = req.body;
+        var newScores= newFriend.scores;
         var similarity = 50;
+        var total = 0;
+        var index = -1;
 
         for (var i = 0; i < friends.length; i++) {
-            var totalDifference = parseInt(0);
+            // var totalDifference = 0;
+            total = 0;
 
-            for (var j = 0; j < newShopper.scores.length; j++) {
-                console.log(totalDifference);
-                totalDifference = parseInt(totalDifference) + Math.abs[parseInt(newShopper.scores[j]) - parseInt(friends[i].scores[j])];
+            // for (var j = 0; j < newShopper.scores.length; j++) {
+                // console.log(totalDifference);
+                // totalDifference = parseInt(totalDifference) + Math.abs[parseInt(newShopper.scores[j]) - parseInt(friends[i].scores[j])];
+
+                for (var j = 0; j < newScores.length; j++) {
+                    var diff = Math.abs(newScores[j] - friends[i].scores[j]);
+                    total += diff;
             }
-            console.log(totalDifference);
+            // console.log(totalDifference);
 
-            if (totalDifference < similarity) {
-                console.log("Friend found");
-                similarity = totalDifference;
-                newFriend = friends[i];
+            // if (totalDifference < similarity) {
+            //     console.log("Friend found");
+            //     similarity = totalDifference;
+            //     newFriend = friends[i];
+            if (total < similarity) {
+                // console.log("Friend found");
+                similarity = total;
+                index = i;
             }
         }
         // adds shopper to friends list
-        friends.push(newShopper);
+        friends.push(newFriend);
 
         // updates friends file
-        fs.writeFileSync(path.join(__dirname, "../model/friends.js"), 'module.exports =' +
-        JSON.stringify(friends, null, 4));
-        console.log(newFriend);
-        res.json(newFriend);
+        // fs.writeFileSync(path.join(__dirname, "../model/friends.js"), 'module.exports = ' +
+        // JSON.stringify(friends, null, 4));
+        // console.log(newFriend);
+        res.json(friends[index]);
+        
     });
 }
